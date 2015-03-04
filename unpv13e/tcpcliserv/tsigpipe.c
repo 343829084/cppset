f90554bf041361f0fe8d0ec7d8aa8050b4b94e47
me115 <me115@126.com>
2015-03-04 10:29:25 +0800

    add unp code

---
+++ unpv13e/tcpcliserv/tsigpipe.c
@@ -0,0 +1,38 @@
+#include	"unp.h"
+
+void
+sig_pipe(int signo)
+{
+	printf("SIGPIPE received\n");
+	return;
+}
+
+int
+main(int argc, char **argv)
+{
+	int					sockfd;
+	struct sockaddr_in	servaddr;
+
+	if (argc != 2)
+		err_quit("usage: tcpcli <IPaddress>");
+
+	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(13);		/* daytime server */
+	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+	Signal(SIGPIPE, sig_pipe);
+
+	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+
+	sleep(2);
+        char * str = "Hello";
+	Write(sockfd, str, 5);
+	sleep(2);
+	char * str2 = "World";
+	Write(sockfd, str2, 5);
+
+	exit(0);
+}
